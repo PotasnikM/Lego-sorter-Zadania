c6649718b3366d022328c044187726280ffc0429
Mateusz <nnn.2321a@gmail.com>
2022-05-11 12:46:51 +0100

    Dodanie zadań

--- Zadania/Zadanie1.py
+++ Zadania/Zadanie1.py
@@ -1,5 +1,5 @@
 """Twoim zadaniem jest stworzenie śmiesznego lub nie obrazka przy pomocy bibliotek Pillow oraz OpenCV.
-1. Pobierz wybrany przez Ceibie obrazek i dodaj go do folderu z zadaniem.
+1. Pobierz wybrany przez Ciebie obrazek i dodaj go do folderu z zadaniem.
 2. Wyświetl go przy pomocy komend imread oraz imshow z biblioteki OpenCV
 3. Wykarz się kreatywnością i edytuj obrazek (zmień kolory/ dodaj filtr / wstaw napis / dorysuj coś / przeskaluj)
    Postaraj się dodać/zmienić conajmniej 3 elementy
